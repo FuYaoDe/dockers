f258170ee12c486d36b880c5e73e183b51172ddb
fuyaode <dan82625@gmail.com>
2015-12-30 20:31:25 +0800

    fix android open port

--- RM-Bridge-Server/install.sh
+++ RM-Bridge-Server/install.sh
@@ -4,12 +4,8 @@ if [ -z "$android_arch" ]
 then
     android_arch="arm"
 fi
-ip=$(ip addr list eth0|grep "inet "|cut -d' ' -f6|cut -d/ -f1)
-redir --laddr=${ip} --lport=5555 --caddr=127.0.0.1 --cport=5555 &
-redir --laddr=${ip} --lport=7474 --caddr=127.0.0.1 --cport=7474 &
-# ls
 
-emulator64-${android_arch} -avd ${android_arch} -http-proxy "$http_proxy" -noaudio -no-window -gpu off -verbose -qemu -vnc :0 &
+emulator64-${android_arch} -avd ${android_arch} -noaudio -no-window -gpu off -verbose -qemu -vnc :0 &
 sleep 120
 adb wait-for-device
 adb install RMBridge_1.2.0.apk
@@ -17,5 +13,11 @@ sleep 60
 adb shell am start -n de.fun2code.android.rmbridge/de.fun2code.android.rmbridge.RMBridgeActivity
 sleep 10
 adb shell input touchscreen tap 240 500
+adb forward tcp:7474 tcp:7474
+
+ip=$(ip addr list eth0|grep "inet "|cut -d' ' -f6|cut -d/ -f1)
+redir --laddr=${ip} --lport=5555 --caddr=127.0.0.1 --cport=5555 &
+redir --laddr=${ip} --lport=7474 --caddr=127.0.0.1 --cport=7474 &
+
 while true; do sleep 1000; done
 # Detect ip and forward ADB ports outside to outside interface
